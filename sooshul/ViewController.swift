4c80c24c2517e2cf6765e5ced4c00132c95e9792
makenaw <makena.walsh@gmail.com>
2016-02-03 12:33:58 +0800

    Facebook is implemented

--- sooshul/ViewController.swift
+++ sooshul/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
@@ -18,6 +20,19 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func fbBtnPressed(sender: UIButton!) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logInWithReadPermissions(["email"]) { (facebookResult: FBSDKLoginManagerLoginResult!, facebookError: NSError!) -> Void in
+            if facebookError != nil {
+                print("Facebook Login Failed Error \(facebookError)")
+            } else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                print("logged in yoru jarmber with facebook \(accessToken)")
+            }
+            
+        }
     }
 
 
