cc87af7b1cbbd21b7c65d4214c97684f7991eb7d
makenaw <makena.walsh@gmail.com>
2016-02-03 11:00:13 +0800

    creates custom view class implementing shadows and rounded corners

---
+++ sooshul/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  sooshul
+//
+//  Created by makena  on 2/3/16.
+//  Copyright © 2016 makena . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
