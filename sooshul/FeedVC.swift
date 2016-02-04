4f18e62dd18f1d493a6c6f9689ceef6d61fca0ba
makenaw <makena.walsh@gmail.com>
2016-02-04 13:12:04 +0800

    adds post model and parses firebase data

--- sooshul/FeedVC.swift
+++ sooshul/FeedVC.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    var posts = [Post]()
     
 
     override func viewDidLoad() {
@@ -18,6 +20,23 @@ class FeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        DataService.ds.REF_POSTS.observeSingleEventOfType(.Value, withBlock: { snapshot in
+            print(snapshot.value)
+            self.posts = []
+            
+            if let snapshots = snapshot.children.allObjects as? [FDataSnapshot] {
+                for snap in snapshots {
+                    print("SNAP \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, dictionary: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
+        })
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -25,10 +44,14 @@ class FeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print(post.postDescription)
+        
         return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
     }
 
---
+++ sooshul/Post.swift
@@ -0,0 +1,53 @@
+//
+//  Post.swift
+//  sooshul
+//
+//  Created by makena  on 2/4/16.
+//  Copyright © 2016 makena . All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    private var _postDescription: String!
+    private var _imageUrl: String?
+    private var _likes: Int!
+    private var _username: String!
+    private var _postkey: String!
+    
+    var postDescription: String {
+        return _postDescription
+    }
+    
+    var imageUrl: String? {
+        return _imageUrl
+    }
+    
+    var likes: Int {
+        return _likes
+    }
+    
+    var username: String {
+        return _username
+    }
+    
+    init(description: String, imageUrl: String?, username: String) {
+        self._postDescription = description
+        self._imageUrl = imageUrl
+        self._username = username
+    }
+    
+    init(postKey: String, dictionary: Dictionary<String, AnyObject>) {
+        self._postkey = postKey
+        if let likes = dictionary["likes"] as? Int {
+            self._likes = likes
+        }
+        if let imageUrl = dictionary["imageUrl"] as? String {
+            self._imageUrl = imageUrl
+        }
+        if let desc = dictionary["description"] as? String {
+            self._postDescription = desc
+        }
+    }
+    
+}
