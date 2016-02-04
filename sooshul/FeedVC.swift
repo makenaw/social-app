e26c64a8851f10c4aae85801d920dad57677618d
makenaw <makena.walsh@gmail.com>
2016-02-04 11:22:58 +0800

    adds tableview cell ui

---
+++ sooshul/FeedVC.swift
@@ -0,0 +1,36 @@
+//
+//  FeedVC.swift
+//  sooshul
+//
+//  Created by makena  on 2/4/16.
+//  Copyright © 2016 makena . All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+    }
+
+
+}
---
+++ sooshul/PostCell.swift
@@ -0,0 +1,33 @@
+//
+//  PostCell.swift
+//  sooshul
+//
+//  Created by makena  on 2/4/16.
+//  Copyright © 2016 makena . All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var showcaseImg: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func drawRect(rect: CGRect) {
+        profileImg.layer.cornerRadius = profileImg.frame.size.width / 2
+        profileImg.clipsToBounds = true
+        
+        showcaseImg.clipsToBounds = true
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
