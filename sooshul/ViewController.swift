195d496357df9deff26b3c28bf6032e1c29724e9
makenaw <makena.walsh@gmail.com>
2016-02-04 12:11:41 +0800

    creating firebase users

--- sooshul/ViewController.swift
+++ sooshul/ViewController.swift
@@ -42,6 +42,10 @@ class ViewController: UIViewController {
                         print("Login failed. \(error)")
                     } else {
                         print("Logged in. \(authData)")
+                        
+                        let user = ["provider": authData.provider!, "blah": "test"]
+                        DataService.ds.createFirebaseUser(authData.uid, user: user)
+                        
                         NSUserDefaults.standardUserDefaults().setValue("authData.uid", forKey: KEY_UID)
                         self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
                     }
@@ -60,7 +64,15 @@ class ViewController: UIViewController {
                                 self.showErrorAlert("Problem creating account", msg: "Could not create account, try something ele")
                             } else {
                                 NSUserDefaults.standardUserDefaults().setValue(result[KEY_UID], forKey: KEY_UID)
-                                DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: nil)
+
+                                DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: { err, authData in
+                                    
+                                    let user = ["provider": authData.provider!, "blah": "emailtest"]
+                                    DataService.ds.createFirebaseUser(authData.uid, user: user)
+                                    
+                                })
+                                
+                                
                                 self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
                             }
                         })
